fd33dfcb353de985a52ca6273408977f51666088
bencrabbe <benoit.crabbe@gmail.com>
2019-10-31 16:30:55 +0100

    added test code

--- DepTree.py
+++ DepTree.py
@@ -287,9 +287,8 @@ class CovingtonParser(nn.Module):
                         mask_val[CovingtonParser.SHIFT] = -float('Inf')
                 
             if len(B) == 1 and i not in graph.has_gov:
-                print(i,j)
-                if i == j-1:
-                    mask_val[CovingtonParser.RIGHT_ARC] = -float('Inf')
+                
+                mask_val[CovingtonParser.RIGHT_ARC] = -float('Inf')
                 mask_val[CovingtonParser.NO_ARC] = -float('Inf')
                 print('Left arc',i,mask_val[CovingtonParser.LEFT_ARC],graph.is_cyclic_add(i,j),graph.is_dag_add(j,i))
                 print('Right arc',mask_val[CovingtonParser.RIGHT_ARC])
