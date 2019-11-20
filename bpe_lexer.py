2ea84e505c6133713be8727d1f7db33507ce1f81
bencrabbe <benoit.crabbe@gmail.com>
2019-11-20 15:54:36 +0100

    params

--- bpe_lexer.py
+++ bpe_lexer.py
@@ -104,7 +104,7 @@ class MultilingualLexer(nn.Module):
         """
         Turns a normal string into a bpe string
         """
-        print(text)
+        print(self.tokenizer.tokenize(text))
         return ' '.join(self.tokenizer.tokenize(text))
         
     def forward(self,bpe_sequence):
