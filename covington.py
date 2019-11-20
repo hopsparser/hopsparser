a06fb7769370c8d62f2f46dd7b3db77a03ab898b
bencrabbe <benoit.crabbe@gmail.com>
2019-11-20 13:10:59 +0100

    params

--- covington.py
+++ covington.py
@@ -403,7 +403,7 @@ if __name__ == "__main__":
     bpe_testset  = DatasetBPE([ ' '.join(graph.words) for graph in test_trees],modelname + '.test-spmrl')  
 
     lexer   = SelectiveBPELexer('bert-base-lowercase/best-valid_fr_mlm_ppl.pth',768)
-    parser  = CovingtonParser(768,768,512,labels,dropout=0.3)   
+    parser  = CovingtonParser(768,768,256,labels,dropout=0.3)   
     parser.train_model(bpe_trainset,train_trees,bpe_validset,valid_trees,lexer,15,learning_rate=0.01,modelname=modelname)
   
     #lexer  = SelectiveBPELexer('frwiki_embed1024_layers12_heads16/model-002.pth',1024)
