7b2c80d9238e423fddd69f0f14f5fb59c470ce26
bencrabbe <benoit.crabbe@gmail.com>
2019-11-23 16:10:26 +0100

    MST

--- mst.py
+++ mst.py
@@ -307,7 +307,7 @@ class GraphParser(nn.Module):
                     deps_embeddings     = input_seq [ torch.tensor( [ dep for (gov,dep) in edgelist ] ) ]
                     label_predictions   = self.label_biaffine(self.dep_lab(deps_embeddings),self.head_lab(gov_embeddings))
                     print(label_predictions)
-                    pred_idxes          = torch.argmax(label_predictions)
+                    pred_idxes          = torch.argmax(label_predictions,dim=1)
                     pred_labels         = [ self.itolab[idx] for idx in pred_idxes ]
                     dg                  = DepGraph([(gov,label,dep) for ((gov,dep),label) in zip(edgelist,pred_labels)],wordlist=wordlist)
                     yield dg
