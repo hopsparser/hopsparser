d8bbc266ca19ff4d3d6e8ef162ff5d4448f5694c
bencrabbe <benoit.crabbe@gmail.com>
2019-11-23 16:51:32 +0100

    MST

--- mst.py
+++ mst.py
@@ -306,11 +306,11 @@ class GraphParser(nn.Module):
                     deps_embeddings   = torch.repeat_interleave(input_seq,repeats=N,dim=0)
                     gov_embeddings    = input_seq.repeat(N,1)
                     attention_scores  = self.edge_biaffine(self.dep_arc(deps_embeddings),self.head_arc(gov_embeddings))
-                    attention_matrix  = logsoftmax(attention_scores.view(N,N)) 
+                    attention_matrix  = logsoftmax(attention_scores.view(N,N)).exp()
+                    print(attention_matrix)
                     #3. Compute max spanning tree
                     M                   = attention_matrix.cpu().numpy()[1:,1:].T                #log-normalize scores ?
                     G                   = nx.from_numpy_matrix(M,create_using=nx.DiGraph)
-                    print(np.exp(M))
                     A                   = nx.maximum_spanning_arborescence(G,default=0)    #this performs a sum
                     #4. Compute edge labels 
                     edgelist            = list(A.edges)
