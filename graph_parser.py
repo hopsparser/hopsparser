9dd528ea0c55fc5375ecb5a0116583e721e00464
bencrabbe <benoit.crabbe@gmail.com>
2019-11-30 12:22:52 +0100

    new biaffine

--- graph_parser.py
+++ graph_parser.py
@@ -329,7 +329,7 @@ class GraphParser(nn.Module):
                     bestNLL = deveNLL+devlNLL
                     self.save_model('test_biaffine.pt2')
                 print('\n  TRAIN: mean NLL(edges)',eNLL/eN,'mean NLL(labels)',lNLL/lN)
-                print('  DEV  : mean NLL(edges)',deveNLL,'mean NLL(labels)',devlNLL)
+                #print('  DEV  : mean NLL(edges)',deveNLL,'mean NLL(labels)',devlNLL)
             except KeyboardInterrupt:
                 print('Received SIGINT. Aborting training.')
                 self.load_state_dict(torch.load('test_biaffine.pt2'))
