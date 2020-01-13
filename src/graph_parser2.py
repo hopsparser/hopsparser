d7c0fe004d4e784ab3bcfd032fc752bae161c851
bencrabbe <benoit.crabbe@gmail.com>
2020-01-13 21:12:33 +0100

    version 2 is now operational

--- src/graph_parser2.py
+++ src/graph_parser2.py
@@ -454,10 +454,10 @@ if __name__ == '__main__':
     mlp_input       = 400 
     mlp_arc_hidden  = 600 
     mlp_lab_hidden  = 100 
-    mlp_dropout     = 0.5
+    mlp_dropout     = 0.3
     device          = "cuda:1" if torch.cuda.is_available() else "cpu"
 
-    trainset           = DependencyDataset('../spmrl/train.French.pred.conll',min_vocab_freq=0,word_dropout=0.3)
+    trainset           = DependencyDataset('../spmrl/train.French.pred.conll',min_vocab_freq=0,word_dropout=0.5)
     itos,itolab,itotag = trainset.itos,trainset.itolab,trainset.itotag
     devset             = DependencyDataset('../spmrl/dev.French.pred.conll',use_vocab=itos,use_labels=itolab,use_tags=itotag)
     testset            = DependencyDataset('../spmrl/test.French.pred.conll',use_vocab=itos,use_labels=itolab,use_tags=itotag)
