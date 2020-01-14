3d45410062a40b1b286e0aabd3a443c3e22b45b1
bencrabbe <benoit.crabbe@gmail.com>
2020-01-14 19:39:15 +0100

    version 2 is now operational

--- src/lexers.py
+++ src/lexers.py
@@ -18,18 +18,20 @@ class DefaultLexer(nn.Module):
         Takes words sequences codes as integer sequences and returns the embeddings 
         """
         return self.embedding(word_sequences)
-
-    
+ 
 class FastTextLexer(nn.Module):
     """
-    This is the a lexer that uses fastText embeddings
+    This is the a lexer that uses fastText embeddings.
+    FastText models are large in memory. 
+    So we intersect the model with the vocabulary found in the data to
+    reduce memory usage. 
     """
     def __init__(self,itos,ft_modelfile='cc.fr.300.bin'):
 
         super(FastTextLexer, self).__init__()
 
         FT = fasttext.load_model(ft_modelfile)
-        self.embedding_size = 300
+        self.embedding_size = 300 #thats the interface property
 
         ematrix = []
         for word in itos:
