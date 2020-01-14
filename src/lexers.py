3e79311092ef3e87a303d256ba06dcdd067b052b
bencrabbe <benoit.crabbe@gmail.com>
2020-01-14 13:38:11 +0100

    version 2 is now operational

---
+++ src/lexers.py
@@ -0,0 +1,21 @@
+from torch import nn
+from graph_parser2 import DependencyDataset
+
+class DefaultLexer(nn.Module):
+    """
+    This is the basic lexer wrapping an embedding layer.
+    """
+    def __init__(self,vocab_size,embedding_size):
+        
+        self.embedding = nn.Embedding(vocab_size, embedding_size, padding_idx=DependencyDataset.PAD_IDX).to(self.device)
+        self.embedding_size = embedding_size #thats the interface property
+        
+    def forward(self,word_sequences):
+        """
+        Takes words sequences codes as integer sequences and returns the embeddings 
+        """
+        return self.embedding(word_sequences)
+
+class FastTextLexer(nn.Module):
+
+    pass
