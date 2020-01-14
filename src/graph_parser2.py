9ef8fd6da4d7152187a39428eb2da4a73fd00596
bencrabbe <benoit.crabbe@gmail.com>
2020-01-14 15:10:58 +0100

    version 2 is now operational

--- src/graph_parser2.py
+++ src/graph_parser2.py
@@ -447,7 +447,7 @@ class BiAffineParser(nn.Module):
                     print(file=ostream)
 
 if __name__ == '__main__':
-    word_embedding_size  = 200
+    word_embedding_size  = 100
     tag_embedding_size   = 100
     encoder_dropout      = 0.3 
     mlp_input            = 400 
@@ -456,10 +456,10 @@ if __name__ == '__main__':
     mlp_dropout          = 0.5
     device               = "cuda:2" if torch.cuda.is_available() else "cpu"
 
-    trainset           = DependencyDataset('../spmrl/train.French.pred.conll',min_vocab_freq=0,word_dropout=0.3)
+    trainset           = DependencyDataset('../spmrl/train.French.gold.conll',min_vocab_freq=0,word_dropout=0.3)
     itos,itolab,itotag = trainset.itos,trainset.itolab,trainset.itotag
-    devset             = DependencyDataset('../spmrl/dev.French.pred.conll',use_vocab=itos,use_labels=itolab,use_tags=itotag)
-    testset            = DependencyDataset('../spmrl/test.French.pred.conll',use_vocab=itos,use_labels=itolab,use_tags=itotag)
+    devset             = DependencyDataset('../spmrl/dev.French.gold.conll',use_vocab=itos,use_labels=itolab,use_tags=itotag)
+    testset            = DependencyDataset('../spmrl/test.French.gold.conll',use_vocab=itos,use_labels=itolab,use_tags=itotag)
     trainset.save_vocab('model.vocab') 
 
     #default lexer
