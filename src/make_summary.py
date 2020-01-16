df07f73f05cabe6034476882e77f83ee79cb0221
bencrabbe <benoit.crabbe@gmail.com>
2020-01-16 10:32:40 +0100

    added summary

--- src/make_summary.py
+++ src/make_summary.py
@@ -1,6 +1,23 @@
+import os
+import sys
 
+def make_csv_summary(dirname,goldfile): 
 
-
+    header  = ''
+    csv_out = open(dirname+'_summary.csv','w')  
+    for filename in os.listdir(dirname):
+        KVlist = filename[:-6].split('-')[1:]
+        if not header:
+            header = [KV.split(':')[0] for KV in KVlist]+['UAS','LAS']
+            print(','.join(header),file=csv_out)
+        values = [KV.split(':')[1] for KV in KVlist]
+        os.system('perl eval07.pl -q -g %s -s %s > tmp/eval.tmp'%(goldfile,filename))
+        (las,uas) =process_eval07(filename)
+        values.append(uas)
+        values.append(las)
+        print(','.join(values),file=csv_out)
+    csv_out.close()
+        
 def process_eval07(filename):
     
     istream = open(filename)
@@ -14,5 +31,4 @@ def process_eval07(filename):
     uas = float(uas[:-2])
     return (las,uas)
 
-
-print(process_eval07('/tmp/eval.tmp'))
+make_csv_summary(sys.argv[0],'../spmrl/test.French.pred.conll')
