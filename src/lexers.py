4d99f1ef30f23c74e1ff4c9314979d2b142d02bc
bencrabbe <benoit.crabbe@gmail.com>
2020-01-19 10:14:03 +0100

    improved bert lexer

--- src/lexers.py
+++ src/lexers.py
@@ -122,7 +122,7 @@ class FlauBertBaseLexer(nn.Module):
         self.bert,_                 = XLMModel.from_pretrained(bert_modelfile, output_loading_info=True)
         self.bert_tokenizer         = XLMTokenizer.from_pretrained(bert_modelfile,\
                                                             do_lowercase_and_remove_accent=False,\
-                                                            additional_special_tokens=[DepGraph.ROOT_TOKEN],\
+                                                            bos_token=DepGraph.ROOT_TOKEN,\
                                                             unk_token=DependencyDataset.UNK_WORD,\
                                                             pad_token=DependencyDataset.PAD_TOKEN)
                                                            #output_hidden_states=True
@@ -166,7 +166,7 @@ class FlauBertBaseLexer(nn.Module):
            a list of integers 
         """
         word_idxes  = [self.stoi.get(token,self.stoi[DependencyDataset.UNK_WORD]) for token in tok_sequence]
-        bert_idxes  = [self.bert_tokenizer.encode(token.lower(),add_special_tokens=False)[0] for token in tok_sequence]
+        bert_idxes  = [self.bert_tokenizer.bos_token_id] +[self.bert_tokenizer.encode(token.lower(),add_special_tokens=False)[0] for token in tok_sequence[1:]]
         print('words',tok_sequence)
         print('word_idxes',word_idxes,[self.itos[idx] for idx in word_idxes])
         print('bert_idxes',bert_idxes,[self.bert_tokenizer.convert_ids_to_tokens(idx) for idx in bert_idxes])
