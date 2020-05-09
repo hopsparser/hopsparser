80144a48bc1966da29a1f9afa36bce77f92bfa52
bencrabbe <benoit.crabbe@gmail.com>
2020-05-09 14:37:34 +0200

    user friendly version

--- src/graph_parser3.py
+++ src/graph_parser3.py
@@ -404,10 +404,11 @@ class BiAffineParser(nn.Module):
                 
                 #FORWARD
                 tag_scores, arc_scores, lab_scores = self.forward(deps)
-                tagger_loss            = loss_fnc(tag_scores,tags)
 
                 print(words, len(words[0]))
                 print(arc_scores.shape, tags.shape)
+
+                tagger_loss            = loss_fnc(tag_scores,tags)
 
                 #ARC LOSS
                 arc_scores = arc_scores.transpose(-1, -2)                           # [batch, sent_len, sent_len]
