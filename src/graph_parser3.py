91e1442fed5b234fc5ea0e7446962d6df0d96718
bencrabbe <benoit.crabbe@gmail.com>
2020-05-10 09:28:46 +0200

    user friendly version

--- src/graph_parser3.py
+++ src/graph_parser3.py
@@ -412,7 +412,7 @@ class BiAffineParser(nn.Module):
 
                 TRAIN_LOSS   += loss.item()
                 
-            scheduler.step()
+            #scheduler.step()
             DEV_LOSS,DEV_ARC_ACC,DEV_LAB_ACC,DEV_TOKS  = self.eval_model(dev_set,batch_size)
             print('Epoch ',e,'train mean loss',TRAIN_LOSS/overall_size,
                              'valid mean loss',DEV_LOSS,
