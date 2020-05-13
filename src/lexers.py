3084b8eb391f277384ba2518426121fbf9d9cdcc
Loïc Grobol <loic.grobol@gmail.com>
2020-05-13 18:36:49 +0200

    Directly use transformers automodels

--- src/lexers.py
+++ src/lexers.py
@@ -2,9 +2,7 @@ import torch
 import fasttext
 from torch import nn
 from graph_parser2 import DependencyDataset,DepGraph
-from transformers  import BertModel, BertTokenizer
-from transformers  import FlaubertModel, FlaubertTokenizer
-from transformers  import CamembertModel,CamembertTokenizer
+from transformers  import AutoModel, AutoTokenizer
 from collections   import Counter,defaultdict
 from random import random
 
@@ -122,24 +120,11 @@ class BertBaseLexer(nn.Module):
         
         self.embedding              = nn.Embedding(len(self.itos), default_embedding_size, padding_idx=DependencyDataset.PAD_IDX)
         
-        if bert_modelfile.startswith('flaubert'):
-            self.bert,_                 = FlaubertModel.from_pretrained(bert_modelfile, output_loading_info=True, output_hidden_states=True)
-            self.bert_tokenizer         = FlaubertTokenizer.from_pretrained(bert_modelfile,\
-                                                                       do_lowercase_and_remove_accent=False,\
-                                                                       unk_token=DependencyDataset.UNK_WORD,\
-                                                                       pad_token=DependencyDataset.PAD_TOKEN)
-                                                                       
-        elif bert_modelfile.startswith('bert'):
-            self.bert,_                 = BertModel.from_pretrained(bert_modelfile, output_loading_info=True, output_hidden_states=True)
-            self.bert_tokenizer         = BertTokenizer.from_pretrained(bert_modelfile,\
-                                                                        do_lowercase_and_remove_accent=False)
-                                                                        
-        elif bert_modelfile.startswith('camembert'):
-            self.bert,_                 = CamembertModel.from_pretrained(bert_modelfile, output_loading_info=True, output_hidden_states=True)
-            self.bert_tokenizer         = CamembertTokenizer.from_pretrained(bert_modelfile,\
-                                                                       do_lowercase_and_remove_accent=False,\
-                                                                       unk_token=DependencyDataset.UNK_WORD,\
-                                                                       pad_token=DependencyDataset.PAD_TOKEN)
+        self.bert,_                 = AutoModel.from_pretrained(bert_modelfile, output_loading_info=True, output_hidden_states=True)
+        self.bert_tokenizer         = AutoTokenizer.from_pretrained(bert_modelfile,
+                                                                    to_lowercase_and_remove_accent=False,
+                                                                    unk_token=DependencyDataset.UNK_WORD,
+                                                                    pad_token=DependencyDataset.PAD_TOKEN)
                                                                        
         
         self.BERT_PAD_IDX = self.bert_tokenizer.pad_token_id
