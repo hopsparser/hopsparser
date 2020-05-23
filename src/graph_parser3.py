ec8e3cd4cfbb2c2658b85cdce2eff9e178cf98d6
bencrabbe <benoit.crabbe@gmail.com>
2020-05-23 18:45:56 +0200

    added char model

--- src/graph_parser3.py
+++ src/graph_parser3.py
@@ -259,7 +259,7 @@ class CharRNN(nn.Module):
 
         super(CharRNN, self).__init__()
 
-        self.word_embedding_size = word_embedding_size
+        self.word_embedding_size = int(word_embedding_size/2) #bc bi-lstm
         self.char_embedding      = nn.Embedding(charset_size, char_embedding_size, padding_idx=DependencyDataset.PAD_IDX)
         self.char_bilstm         = nn.LSTM(char_embedding_size,\
                                            word_embedding_size,1,\
