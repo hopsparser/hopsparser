aaae5397b6720f7d3b155b9df8ef53678d2d463d
bencrabbe <benoit.crabbe@gmail.com>
2020-06-01 17:55:06 +0200

    change encoder arch

--- src/graph_parser3.py
+++ src/graph_parser3.py
@@ -341,8 +341,8 @@ class BiAffineParser(nn.Module):
         super(BiAffineParser, self).__init__()
         self.device            = torch.device(device) if type(device) == str else device
         self.lexer             = lexer.to(self.device)
-        self.tag_rnn           = nn.LSTM(self.lexer.embedding_size+char_rnn.word_embedding_size,mlp_input,1, batch_first=True,dropout=encoder_dropout,bidirectional=True).to(self.device)
-        self.dep_rnn           = nn.LSTM(tagset_size+self.lexer.embedding_size+char_rnn.word_embedding_size,mlp_input,2, batch_first=True,dropout=encoder_dropout,bidirectional=True).to(self.device)
+        self.tag_rnn           = nn.LSTM(self.lexer.embedding_size+char_rnn.word_embedding_size,mlp_input,2, batch_first=True,dropout=encoder_dropout,bidirectional=True).to(self.device)
+        self.dep_rnn           = nn.LSTM(tagset_size+self.lexer.embedding_size+char_rnn.word_embedding_size,mlp_input,3, batch_first=True,dropout=encoder_dropout,bidirectional=True).to(self.device)
 
         #POS tagger & char RNN
         #self.pos_tagger    = Tagger(mlp_input*2,tagset_size).to(self.device)
