1734a349e5d0c91d3138deb0fd0b6c678b42ef6f
bcrabbe <benoit.crabbe@gmail.com>
2020-06-14 07:28:00 +0200

    redesigned the fasttext lexer

--- src/graph_parser3.py
+++ src/graph_parser3.py
@@ -380,7 +380,7 @@ class FastTextTorch(nn.Module):
             source_stream.close()
 
             print('Training fasttext model...')
-            model = fasttext.train_unsupervised(source_file, model='skipgram',minCount=0,epoch=10)
+            model = fasttext.train_unsupervised(source_file, model='skipgram',minCount=1,epoch=500)
             model.save_model(target_file)
         return FastTextTorch(model)
 
