d6924d8887dfb2ebf9a1a522897bd76c50885781
Loïc Grobol <loic.grobol@gmail.com>
2021-02-13 18:16:50 +0100

    programmatic API doc

--- npdependency/server.py
+++ npdependency/server.py
@@ -1,8 +1,9 @@
 import io
-from typing import Dict, Literal, Optional
+from typing import Dict, List, Literal, Optional
 import fastapi
 import pydantic
 
+from npdependency import __version__
 from npdependency import deptree, graph_parser, lexers
 
 
@@ -24,28 +25,57 @@ default_model = next(iter(models.keys()))
 
 
 class ParseRequest(pydantic.BaseModel):
-    data: str
-    input: Literal["conllu", "horizontal"] = "conllu"
-    model: str = default_model
-    output: Literal["conllu"] = "conllu"
-    parser: Optional[str] = None
-    tagger: Optional[str] = None
-    tokenizer: Optional[str] = None
+    data: str = pydantic.Field(
+        ...,
+        description="Input data in UTF-8.",
+        example="Je reconnais l'existence du kiwi.",
+    )
+    input: Literal["conllu", "horizontal"] = pydantic.Field(
+        "conllu", description="Input data in UTF-8."
+    )
+    model: str = pydantic.Field(
+        default_model,
+        description="The model to use to parse the data. See `models` for a list.",
+    )
+    output: Literal["conllu"] = pydantic.Field(
+        "conllu", description="The output format to use."
+    )
+    parser: Optional[str] = pydantic.Field(
+        None, description="Ignored (for compatibility with UDPipe.)"
+    )
+    tagger: Optional[str] = pydantic.Field(
+        None, description="Ignored (for compatibility with UDPipe.)"
+    )
+    tokenizer: Optional[str] = pydantic.Field(
+        None, description="Ignored (for compatibility with UDPipe.)"
+    )
 
 
-app = fastapi.FastAPI()
+class ParseResponse(pydantic.BaseModel):
+    model: str = pydantic.Field(..., description="The model used to parse the data.")
+    acknowledgements: List[str] = pydantic.Field(
+        ..., description="A list of acknowledgements for the model used."
+    )
+    data: str = pydantic.Field(..., description="The processed output.")
 
 
-@app.get("/models")
-async def get_model():
+app = fastapi.FastAPI(
+    title="HOPSparser REST endpoint",
+    description="A honest parser of sentences.",
+    version=__version__,
+)
+
+
+@app.get("/models", summary="Enumerate the loaded models.")
+async def get_models():
     return {
         "models": {m: ["tagger", "parser"] for m in models.keys()},
         "default_model": default_model,
     }
 
 
-@app.post("/process")
-async def process(req: ParseRequest):
+@app.post("/process", summary="Parse data.", response_model=ParseResponse)
+async def process(req: ParseRequest) -> ParseResponse:
     model_name = req.model if req.model is not None else next(iter(models.keys()))
     parser = models.get(model_name)
     if parser is None:
@@ -80,4 +110,6 @@ async def process(req: ParseRequest):
     )
     parsed = io.StringIO()
     parser.predict_batch(treebank, parsed, greedy=False)
-    return {"model": model_name, "acknowledgements": [""], "result": parsed.getvalue()}
+    return ParseResponse(
+        model=model_name, acknowledgements=[""], result=parsed.getvalue()
+    )
