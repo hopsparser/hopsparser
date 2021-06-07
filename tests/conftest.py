93f9b4160ef8f64007959dcc6ce08c6800b93b2e
Loïc Grobol <loic.grobol@gmail.com>
2021-06-07 23:54:31 +0200

    🎉

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import pathlib
+import pytest
+
+
+@pytest.fixture
+def test_data_dir() -> pathlib.Path:
+    return pathlib.Path(__file__).parent / "fixtures"
+
+
+@pytest.fixture
+def treebank(test_data_dir: pathlib.Path) -> pathlib.Path:
+    return test_data_dir / "truncated-sv_talbanken-ud-dev.conllu"
+
+
+@pytest.fixture
+def raw_text(test_data_dir: pathlib.Path) -> pathlib.Path:
+    return test_data_dir / "raw.txt"
+
+
+@pytest.fixture(params=["toy_nobert", "toy_flaubert"])
+def train_config(test_data_dir: pathlib.Path, request) -> pathlib.Path:
+    return test_data_dir / f"{request.param}.yaml"
---
+++ tests/test_smoke.py
@@ -0,0 +1,93 @@
+import filecmp
+import pathlib
+
+import pytest_console_scripts
+
+
+def test_train_parse(
+    raw_text: pathlib.Path,
+    script_runner: pytest_console_scripts.ScriptRunner,
+    tmp_path: pathlib.Path,
+    train_config: pathlib.Path,
+    treebank: pathlib.Path,
+):
+    ret = script_runner.run(
+        "hopsparser",
+        "train",
+        str(train_config),
+        str(treebank),
+        str(tmp_path),
+        "--dev-file",
+        str(treebank),
+        "--test-file",
+        str(treebank),
+    )
+    assert ret.success
+    ret = script_runner.run(
+        "hopsparser",
+        "parse",
+        str(tmp_path / "model"),
+        str(treebank),
+        str(tmp_path / f"{treebank.stem}.parsed2.conllu"),
+    )
+    assert ret.success
+    assert filecmp.cmp(
+        tmp_path / f"{treebank.stem}.parsed.conllu",
+        tmp_path / f"{treebank.stem}.parsed2.conllu",
+        shallow=False,
+    )
+    ret = script_runner.run(
+        "hopsparser",
+        "parse",
+        "--raw",
+        str(tmp_path / "model"),
+        str(raw_text),
+        str(tmp_path / f"{raw_text.stem}.parsed.conllu"),
+    )
+    assert ret.success
+    ret = script_runner.run(
+        "hopsparser",
+        "parse",
+        str(tmp_path / "model"),
+        str(tmp_path / f"{raw_text.stem}.parsed.conllu"),
+        str(tmp_path / f"{raw_text.stem}.reparsed.conllu"),
+    )
+    assert ret.success
+    assert filecmp.cmp(
+        str(tmp_path / f"{raw_text.stem}.parsed.conllu"),
+        str(tmp_path / f"{raw_text.stem}.reparsed.conllu"),
+        shallow=False,
+    )
+
+
+def test_graph_parser(
+    train_config: pathlib.Path,
+    treebank: pathlib.Path,
+    script_runner: pytest_console_scripts.ScriptRunner,
+    tmp_path: pathlib.Path,
+):
+    ret = script_runner.run(
+        "graph_parser",
+        str(train_config),
+        "--train_file",
+        str(treebank),
+        "--dev_file",
+        str(treebank),
+        "--pred_file",
+        str(treebank),
+        "--out_dir",
+        str(tmp_path),
+    )
+    assert ret.success
+
+
+def test_gold_evaluation(
+    script_runner: pytest_console_scripts.ScriptRunner, treebank: pathlib.Path
+):
+    ret = script_runner.run(
+        "eval_parse",
+        "-v",
+        str(treebank),
+        str(treebank),
+    )
+    assert ret.success
