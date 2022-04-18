1eca239d0cf6a7445ad4b0b3e3033ed93bc70e16
Loïc Grobol <loic.grobol@gmail.com>
2022-04-18 16:11:48 +0200

    text: ensure that parsing scores are batching-invariant
    So the same sentence is always parsed the same way no matter what it is
    batched with

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,16 +1,17 @@
 import pathlib
 import tempfile
 from typing import List, Tuple
-from numpy import source
 
 import torch.cuda
 
 import hypothesis.strategies as st
 import pytest
-from hypothesis import given, settings
+from hypothesis import assume, given, settings
+from torch.testing import assert_close
 
 from hopsparser.parser import BiAffineParser
 from hopsparser.deptree import DepGraph
+from hopsparser.lexers import LexingError
 
 devices = ["cpu"]
 if torch.cuda.is_available():
@@ -57,6 +58,90 @@ def parser_and_reload(
         parser.save(tmp_path, save_weights=True)
         reloaded = BiAffineParser.load(tmp_path)
     return parser, reloaded
+
+
+# FIXME: this should be generated in hypothesis to allow config variation
+@pytest.fixture(scope="session")
+def parser(
+    train_config: pathlib.Path,
+    treebank: pathlib.Path,
+):
+    parser = BiAffineParser.initialize(
+        config_path=train_config,
+        treebank=list(DepGraph.read_conll(open(treebank))),
+    )
+    return parser
+
+
+@pytest.mark.parametrize("device", devices)
+@settings(deadline=8192)
+# FIXME: should we really skip control characters and whitespaces? We do now because most 🤗
+# tokenizers strip them out instead of rendering them as unk (see also test_lexers)
+@given(
+    stable_text=st.lists(
+        st.text(alphabet=st.characters(blacklist_categories=["Zs", "C"]), min_size=1),
+        min_size=1, max_size=32,
+    ),
+    distractor_text_1=st.lists(
+        st.text(alphabet=st.characters(blacklist_categories=["Zs", "C"]), min_size=1),
+        min_size=1, max_size=32,
+    ),
+    distractor_text_2=st.lists(
+        st.text(alphabet=st.characters(blacklist_categories=["Zs", "C"]), min_size=1),
+        min_size=1, max_size=32,
+    ),
+)
+def test_batch_invariance(
+    device: str,
+    parser: BiAffineParser,
+    stable_text: List[str],
+    distractor_text_1: List[str],
+    distractor_text_2: List[str],
+):
+    parser = parser.to(device)
+    parser.eval()
+    try:
+        encoded_stable_text = parser.encode_sentence(stable_text, strict=True)
+        encoded_distractor_text_1 = parser.encode_sentence(
+            distractor_text_1, strict=True
+        )
+        encoded_distractor_text_2 = parser.encode_sentence(
+            distractor_text_2, strict=True
+        )
+    except LexingError:
+        assume(False)
+    with torch.no_grad():
+        stable_length = len(stable_text) + 1
+        batch_stable = parser.batch_sentences([encoded_stable_text])
+        text_s1 = [encoded_stable_text, encoded_distractor_text_1]
+        text_1s = [encoded_distractor_text_1, encoded_stable_text]
+        text_s2 = [encoded_stable_text, encoded_distractor_text_2]
+        ref_tagger_scores: torch.Tensor
+        ref_arc_scores: torch.Tensor
+        ref_lab_scores: torch.Tensor
+        ref_tagger_scores, ref_arc_scores, ref_lab_scores = parser(
+            batch_stable.encodings, batch_stable.sent_lengths
+        )
+        for text, idx in ((text_s1, 0), (text_1s, 1), (text_s2, 0)):
+            batch = parser.batch_sentences(text)
+            tagger_scores: torch.Tensor
+            arc_scores: torch.Tensor
+            lab_scores: torch.Tensor
+            tagger_scores, arc_scores, lab_scores = parser(
+                batch.encodings, batch.sent_lengths
+            )
+            assert_close(
+                ref_tagger_scores[0, :stable_length, :],
+                tagger_scores[idx, :stable_length, :],
+            )
+            assert_close(
+                ref_arc_scores[0, :stable_length, :stable_length],
+                arc_scores[idx, :stable_length, :stable_length],
+            )
+            assert_close(
+                ref_lab_scores[0, :stable_length, :stable_length, :],
+                lab_scores[idx, :stable_length, :stable_length, :],
+            )
 
 
 @pytest.mark.parametrize("device", devices)
