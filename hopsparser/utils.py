4d522959f4a6a6038e9e649bd7ca8f96f1dbc8f7
Loïc Grobol <loic.grobol@gmail.com>
2022-05-03 19:19:52 +0200

    fix: Capture transformers warning in logger with the main API

--- hopsparser/utils.py
+++ hopsparser/utils.py
@@ -10,6 +10,7 @@ from typing import IO, Dict, Generator, Optional, Union, cast
 from loguru import logger
 import rich.text
 import rich.progress
+import transformers
 
 
 @contextlib.contextmanager
@@ -107,6 +108,25 @@ def setup_logging(verbose: bool, logfile: Optional[pathlib.Path] = None):
             ),
             colorize=False,
         )
+    
+    class InterceptHandler(logging.Handler):
+        def emit(self, record):
+            # Get corresponding Loguru level if it exists
+            try:
+                level = logger.level(record.levelname).name
+            except ValueError:
+                level = record.levelno
+
+            # Find caller from where originated the logged message
+            frame, depth = logging.currentframe(), 2
+            while frame.f_code.co_filename == logging.__file__:
+                frame = frame.f_back
+                depth += 1
+
+            logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+    transformers.utils.logging.disable_default_handler
+    transformers.utils.logging.add_handler(InterceptHandler())
 
 
 def log_epoch(epoch_name: str, metrics: Dict[str, str]):
