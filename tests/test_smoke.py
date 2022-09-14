5654f462562fa63ee9bedfd3c9090754bf9f937a
Loïc Grobol <loic.grobol@gmail.com>
2022-09-14 17:10:53 +0200

    test: smoke test for train_multi

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -85,6 +85,98 @@ def test_train_parse(
     )
 
 
+@pytest.mark.parametrize("device", devices)
+def test_train_multi_parse(
+    device: str,
+    raw_text: pathlib.Path,
+    script_runner: pytest_console_scripts.ScriptRunner,
+    tmp_path: pathlib.Path,
+    train_config: pathlib.Path,
+    treebank: pathlib.Path,
+    test_treebank: pathlib.Path,
+):
+    ret = script_runner.run(
+        "hopsparser",
+        "train-multi",
+        "--device",
+        device,
+        str(train_config),
+        f"one:{treebank}",
+        f"two:{treebank}",
+        str(tmp_path),
+        "--dev-file",
+        f"one:{test_treebank}",
+        "--dev-file",
+        f"two:{test_treebank}",
+        "--dev-file",
+        f"three:{test_treebank}",
+        "--test-file",
+        f"one:{test_treebank}",
+        "--test-file",
+        f"three:{test_treebank}",
+        "--test-file",
+        f"four:{test_treebank}",
+    )
+    assert ret.success
+    ret = script_runner.run(
+        "eval_parse",
+        "-v",
+        str(tmp_path / f"one-{test_treebank.stem}.parsed.conllu"),
+        str(test_treebank),
+    )
+    assert ret.success
+    ret = script_runner.run(
+        "eval_parse",
+        "-v",
+        str(tmp_path / f"three-{test_treebank.stem}.parsed.conllu"),
+        str(test_treebank),
+    )
+    assert ret.success
+    ret = script_runner.run(
+        "eval_parse",
+        "-v",
+        str(tmp_path / f"four-{test_treebank.stem}.parsed.conllu"),
+        str(test_treebank),
+    )
+    assert ret.success
+    ret = script_runner.run(
+        "hopsparser",
+        "parse",
+        "--device",
+        device,
+        str(tmp_path / "model"),
+        str(treebank),
+        str(tmp_path / f"{test_treebank.stem}.parsed2.conllu"),
+    )
+    assert ret.success
+    ret = script_runner.run(
+        "hopsparser",
+        "parse",
+        "--device",
+        device,
+        "--raw",
+        str(tmp_path / "model"),
+        str(raw_text),
+        str(tmp_path / f"{raw_text.stem}.parsed.conllu"),
+    )
+    assert ret.success
+    ret = script_runner.run(
+        "hopsparser",
+        "parse",
+        "--device",
+        device,
+        str(tmp_path / "model"),
+        str(tmp_path / f"{raw_text.stem}.parsed.conllu"),
+        str(tmp_path / f"{raw_text.stem}.reparsed.conllu"),
+    )
+    assert ret.success
+    assert filecmp.cmp(
+        str(tmp_path / f"{raw_text.stem}.parsed.conllu"),
+        str(tmp_path / f"{raw_text.stem}.reparsed.conllu"),
+        shallow=False,
+    )
+
+
 def test_gold_evaluation(
     script_runner: pytest_console_scripts.ScriptRunner, test_treebank: pathlib.Path
 ):
