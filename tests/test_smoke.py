4b65b7fe4e3b800aec8889fdac8b74b0de0cb4a1
Loïc Grobol <loic.grobol@gmail.com>
2025-02-12 19:45:03 +0100

    fix smoke test

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -227,7 +227,7 @@ def test_train_script(
         [
             "python",
             str(scripts_dir / "train_models.py"),
-            str(test_data_dir / "train_script" / "train_configs"),
+            str(test_data_dir / "train_script" / "train_config.yaml"),
             str(test_data_dir / "train_script" / "treebanks"),
             "--devices",
             f"{device},{device}",
