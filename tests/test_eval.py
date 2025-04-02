676d4c1c16cdb738e1b80b3728e3cef8727600e5
Loïc Grobol <loic.grobol@gmail.com>
2025-04-03 00:13:04 +0200

    better, faster, Prüfer sequences

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -1,5 +1,5 @@
 import heapq
-from typing import Iterable, Sequence
+from typing import Iterable, Sequence, cast
 import numpy as np
 import pytest
 from hopsparser.conll2018_eval import UDError
@@ -9,75 +9,98 @@ from hypothesis import assume, given
 from hypothesis import strategies as st
 
 
-# This might be made more efficient by figuring out an alternative Prüfer scheme that directly gets
-# the arcs with the right orientation????
 def seq_to_heads(
     seq: np.ndarray[tuple[int], np.dtype[np.intp]], root: np.intp
 ) -> np.ndarray[tuple[int], np.dtype[np.intp]]:
-    """Given a Prüfer sequence for a tree and a root, return the corresponding arborescence as an
-    array `heads` encoding all the `(i, heads[i])` directed arcs. `heads[root]` is set to -1."""
+    """Given a Prüfer sequence for a rooted tree, return the corresponding arborescence as an
+    array `heads` encoding all the `(i, heads[i])` directed arcs. `heads[root]` is set to -1.
+
+    The Prüfer sequence for a rooted tree is build exactly like a regular Prüfer sequence, except
+    that at each step, instead of removing the leaf with the lowest index, you remove the leaf with
+    the lowest index *that is not the root*.
+    """
     n = seq.shape[0] + 2
+
     degrees = np.ones(n, dtype=np.intp)
     values, counts = np.unique_counts(seq)
     degrees[values] += counts
-    # Need a list for heapification. You'd THINK we could store degrees - 1 to make this terser but
-    # actually numpy doesn't have a zero_loc function lol.
-    leaves = np.flatnonzero(degrees == 1).tolist()
+    # This is not actually the degree of the root, but this way it can't never enter the leaves heap
+    degrees[root] = 0
+    # We need a list for heapification. You'd THINK we could store degrees - 1 to make this terser
+    # but actually numpy doesn't have a zero_loc function lol. The type is a union because at first
+    # we'll get Python int but later we won't be bothering casting the np int we'll be getting.
+    leaves = cast(list[int | np.intp], np.flatnonzero(degrees == 1).tolist())
     # that's a min-heap, ty Python
     heapq.heapify(leaves)
 
-    # First determine the arcs for the case where the root is n-1, orienting them as `(i, arc[i])`
-    arcs = np.full(n - 1, fill_value=n - 1, dtype=np.intp)
+    arcs = np.full(n, fill_value=-1, dtype=np.intp)
 
+    # We are replaying the sequence building: at each step, the node `i` that was popped and whose
+    # sole neighbour `j` got put in the sequence was the leaf with the lowest index that is not the
+    # root. So if we see `j` in the sequence, we only need to find `i` in the same way.
     for head in seq:
         leaf = heapq.heappop(leaves)
         arcs[leaf] = head
+        # We have seen all the neighbours of `head` but one, and it is not the root (otherwise its
+        # "degree" would be 0, so it becomes a leaf.
         if degrees[head] == 2:
             # Slightly less efficient than heapreplace but more legible
             heapq.heappush(leaves, head)
-        else:
+        elif head != root:
             degrees[head] -= 1
 
-    # The arcs here give us the correct heads for a root at `n-1`: at this point the neighbours of a
-    # non-root node `i` are exactly `arcs[i]` and the nodes `j` such that `arcs[j] == i`. Now if we
-    # say that a non-root node `i` is *correctly oriented* iff `arcs[i]` is its head in the
-    # `n-1`-rooted arborescence, then:
+    # There's two nodes left unpopped after building the Prüfer sequence: the root, and that last
+    # leaf that's still in the heap and whose head can only be the root
+    arcs[leaves[0]] = root
+
+    #  The arcs here give us the correct heads for a root at `n-1`: at
+    # this point the neighbours of a non-root node `i` are exactly `arcs[i]` and the nodes `j` such
+    # that `arcs[j] == i`. Now if we say that a non-root node `i` is *correctly oriented* iff
+    # `arcs[i]` is its head in the `n-1`-rooted arborescence, then:
     # - Every child `i` of `n-1` is correctly oriented: since it's a neighbour of `n-1` and
     #   `arcs[n-1]` doesn't exist, we must have `arcs[i] == n-1`.
     # - If a node `i` is correctly oriented, then for every child `j` of `i`, since `j` is neighbour
     #   of `i` and `arcs[i] != j` (because `j` is not the head of `i`), we must have `arcs[j] == i`,
-    #   and therefore `j` is correctly oriented.
-    # And therefore, by induction, all the nodes are correctly oriented.
+    #   and therefore `j` is correctly oriented. And therefore, by induction, all the nodes are
+    # correctly oriented.
 
-    if root == n - 1:
-        return np.append(arcs, [-1])
+    return arcs
 
-    # Re-orient the arcs with a breadth-first search
 
-    heads = np.full(n, fill_value=-1, dtype=np.intp)
+def heads_to_seq(
+    heads: np.ndarray[tuple[int], np.dtype[np.intp]], root: np.intp
+) -> np.ndarray[tuple[int], np.dtype[np.intp]]:
+    """Given an arborescence as an array `heads` encoding all the `(i, heads[i])` directed arcs,
+    return the corresponding Prüfer sequence. `heads[root]` must be `-1`.
 
-    # special-casing the root case is slightly more efficient
-    children = np.zeros_like(heads, dtype=np.bool)
-    children[np.flatnonzero(arcs == root)] = True
-    children[arcs[root]] = True
-    heads[children] = root
-    opened = np.flatnonzero(children).tolist()
-    heapq.heapify(opened)
+    The Prüfer sequence for a rooted tree is build exactly like a regular Prüfer sequence, except
+    that at each step, instead of removing the leaf with the lowest index, you remove the leaf with
+    the lowest index *that is not the root*.
+    """
+    n = heads.shape[0]
+    degrees = np.ones(n, dtype=np.intp)
+    values, counts = np.unique_counts(heads)
+    # Since the root is -1 and the values are sorted, we can safely escape this way
+    degrees[values[1:]] += counts[1:]
+    degrees[root] = 0
+    leaves = cast(list[int], np.flatnonzero(degrees == 1).tolist())
+    heapq.heapify(leaves)
 
-    while opened:
-        current = heapq.heappop(opened)
-        children.fill(False)
-        children[np.flatnonzero(arcs == current)] = True
-        if current != n - 1:
-            children[arcs[current]] = True
-        children[heads[current]] = False
-        children_idx = np.flatnonzero(children)
-        heads[children_idx] = current
-        # we have no better heapextend method
-        opened.extend(children_idx)
-        heapq.heapify(opened)
+    seq = np.empty(n - 2, dtype=np.intp)
 
-    return heads
+    for i in range(n - 2):
+        leaf = heapq.heappop(leaves)
+        h = heads[leaf]
+        seq[i] = h
+        # We have seen all the neighbours of `head` but one, and it is not the root (otherwise its
+        # "degree" would be 0, so it becomes a leaf.
+        if degrees[h] == 2:
+            # Slightly less efficient than heapreplace but more legible
+            heapq.heappush(leaves, h)
+        elif h != root:
+            degrees[h] -= 1
+
+    return seq
 
 
 tokens_strat = st.text(
