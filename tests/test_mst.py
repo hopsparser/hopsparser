012f7598430a9a32ff7545e1615863afd3f09ecb
Loïc Grobol <loic.grobol@gmail.com>
2025-04-29 00:48:22 +0200

    reduce max test size for cle_one_root bc it's too slooow

--- tests/test_mst.py
+++ tests/test_mst.py
@@ -3,17 +3,20 @@ import math
 import networkx as nx
 import numpy as np
 from numpy.testing import assert_allclose
-from hypothesis import given, reproduce_failure, settings
+from hypothesis import given, settings
 from hypothesis import strategies as st
 from hypothesis.extra.numpy import arrays
 
 from hopsparser import mst
 
 
-# Only positive weights becaux nx seems to have issues with negative ones. We also set a lower bound
-# that's not too small, because nx overwrites (????) the original weigths in G with `weight +
-# min_weight - 1 + (min_weight - max_weight)`, which clamps to 0 if the weight is something too
-# small, like 2.225074e-308 or even 1e-16.
+# Only positive weights becaux nx seems to have issues with negative ones.
+#
+# We also set a lower bound that's not too small, because nx overwrites (????) the original weigths
+# in G with `weight + min_weight - 1 + (min_weight - max_weight)`, which clamps to 0 if the weight
+# is something too small, like 2.225074e-308 or even 1e-16.
+#
+# TODO: report this as a bug to nx
 @settings(deadline=1024)
 @given(
     adjacency=arrays(
@@ -43,7 +46,6 @@ def test_cle(adjacency: np.ndarray[tuple[int, int], np.dtype[np.float64]]):
     nx_arborescence = nx.algorithms.tree.branchings.maximum_spanning_arborescence(
         graph,
         attr="weight",
-        preserve_attrs=True,
     )
     nx_weight = np.sum([nx_arborescence.get_edge_data(*e)["weight"] for e in nx_arborescence.edges])
 
@@ -57,7 +59,7 @@ def test_cle(adjacency: np.ndarray[tuple[int, int], np.dtype[np.float64]]):
 @given(
     adjacency=arrays(
         dtype=np.float64,
-        shape=st.integers(min_value=2, max_value=128).map(lambda x: (x, x)),
+        shape=st.integers(min_value=2, max_value=64).map(lambda x: (x, x)),
         elements=st.floats(
             allow_nan=False,
             allow_infinity=False,
@@ -66,11 +68,6 @@ def test_cle(adjacency: np.ndarray[tuple[int, int], np.dtype[np.float64]]):
     ),
 )
 def test_cle_one_root(adjacency: np.ndarray[tuple[int, int], np.dtype[np.float64]]):
-    # Parsing setting: no self-loop, 0 is the root
-    np.fill_diagonal(adjacency, -np.inf)
-    adjacency[0, 1:] = -np.inf
-    adjacency[0, 0] = 0.0
-
     mst_heads = mst.chuliu_edmonds_one_root(adjacency)
 
     # No cycle
